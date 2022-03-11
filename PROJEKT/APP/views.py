53cb28639a45ff142969b0aea089a101f3bebb1d
Farkas Ükös <ukosfarkas@gmail.com>
2022-03-11 13:04:48 +0100

    ad

--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,3 +1,16 @@
+from re import template
 from django.shortcuts import render
-
+from random import random, randint
+from .models import Kerdes
 # Create your views here.
+
+def bigyoview (request):
+    kerdesek = Kerdes.objects.all()
+
+    # for kerdes in kerdesek:
+    #     print(kerdes)
+
+
+    template = 'bigyotemplate.html'
+    context = {'a' : randint(0,10), 'kerdesek': kerdesek}
+    return render(request,  template, context)
